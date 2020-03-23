8659663c86aa6cd4edc54ef73fc1bafc39b5e26b
alos <alexander.oswald@web.de>
2020-03-23 23:29:16 +0100

    initial commit

---
+++ psUnzip.ps1
@@ -0,0 +1,4 @@
+﻿$archives = gci -Recurse -filter *.zip
+foreach ($archives in $archives){Expand-Archive -Path $archives -DestinationPath $archives.Directory -force}
+$archives = gci -Recurse -filter *.zip
+Remove-Item -Path $archives
