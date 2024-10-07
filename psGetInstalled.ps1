6fd8b0a8ebef970ef4332c3776ea5c5b10c46d6c
alos-source <alexander.oswald@web.de>
2024-10-07 18:01:54 +0200

    add get installed software

---
+++ psGetInstalled.ps1
@@ -0,0 +1,5 @@
+﻿Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*,
+HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |
+Sort-Object DisplayName |
+Format-Table -AutoSize
